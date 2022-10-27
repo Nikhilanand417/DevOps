11ce27e851502fc628b58801181f484c40395527
Nikhilanand <nikhilanand417@gmail.com>
2022-10-27 15:06:31 -0400

    add frontend

---
+++ robot/components/common.sh
@@ -0,0 +1,16 @@
+LOGFILE = /tmp/$Component.log
+USER_ID = (uid -u)
+
+#validation for root user
+if[ $USER_ID -ne 0 ] ;then
+    echo -e "\e[31m You must execute as a Root User\e[0m"
+    exit 1
+fi
+
+stat(){
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m Success\e[0m"
+    else
+        echo -e "\e[31m Failed\e[0m"
+    fi
+}
--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
+set -e 
+
+Component = frontend
+source = components/common.sh
+
+
+echo -n " Installing Nginx"
+yum install nginx -yum &>> $LOGFILE
+stat $?
+
+echo -n " Downloading Nginx"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+stat $?
+
+echo -n " Cleaning the directory "
+cd /usr/share/nginx/html
+rm -rf *
+stat $?
+
+echo -n " Deploying in default location"
+unzip /tmp/frontend.zip &>> $LOGFILE
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md $LOGFILE
+stat $?
+
+echo -n " configuring proxy "
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+echo -n "starting the service"
+systemctl enable nginx
+systemctl start nginx
+stat $?
 
