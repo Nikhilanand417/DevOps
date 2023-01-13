3d4f8ca87fa3aa32bb75046477c082374a8e12f0
Nikhilanand <nikhilanand417@gmail.com>
2023-01-13 13:48:06 -0500

    Added file

--- robot/Shellscripting.sh
+++ robot/Shellscripting.sh
@@ -4,6 +4,7 @@ a=10
 b=20
 c=$1
 Today="$(date +%F)"
+Load="$(uptime | awk -F : '{print $NF}' | awk -F , '{print $3}')"
 Sample(){
     
     echo -e "\e[33m Todays date is $Today\e[0m"
@@ -13,4 +14,6 @@ Sample
 echo $$
 
 read -p 'Enter the name': name 
-echo "name is $name" 
+echo "name is $name"
+
+echo " The load average of the server is $Load"
